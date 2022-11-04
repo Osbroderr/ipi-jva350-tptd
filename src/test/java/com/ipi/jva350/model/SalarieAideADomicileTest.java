f85919ee4347e72c658e20c7fee1b596e59a65fe
Your Name <you@example.com>
2022-11-04 12:18:05 +0100

    test cas au limite calculJoursDeCongePourPlageParam

--- src/test/java/com/ipi/jva350/model/SalarieAideADomicileTest.java
+++ src/test/java/com/ipi/jva350/model/SalarieAideADomicileTest.java
@@ -1,12 +1,11 @@
 package com.ipi.jva350.model;
 
-import static org.mockito.ArgumentMatchers.contains;
-
 import java.time.LocalDate;
 import java.util.LinkedHashSet;
-import java.util.Set;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.api.Assertions;
 
 public class SalarieAideADomicileTest {
@@ -49,17 +48,39 @@ public class SalarieAideADomicileTest {
         Assertions.assertEquals(false, res1);
     }
 
-    // @Test
-    // void testcalculeJoursDeCongeDecomptesPourPlage() {
-    // // Given :
-    // SalarieAideADomicile aide = new SalarieAideADomicile("Jeanne",
-    // LocalDate.now(), LocalDate.now(), 0, 0, 10, 1,
-    // 1);
-    // // When :
-    // Set<LocalDate> res =
-    // aide.calculeJoursDeCongeDecomptesPourPlage(LocalDate.of(2021, 01, 21),
-    // LocalDate.now());
-    // // Then :
-    // Assertions.assertEquals(true, res.isEmpty());
-    // }
+    @Test
+    void testcalculeJoursDeCongeDecomptesPourPlageEgal() {
+        // Given :
+        SalarieAideADomicile aide = new SalarieAideADomicile("Jeanne",
+                LocalDate.now(), LocalDate.now(), 0, 0, 10, 1,
+                1);
+        // When :
+        LinkedHashSet<LocalDate> res = aide.calculeJoursDeCongeDecomptesPourPlage(LocalDate.of(2022, 7, 01),
+                LocalDate.of(2022, 7, 02));
+
+        // Then :
+        LinkedHashSet<LocalDate> expected = new LinkedHashSet<>();
+        expected.add(LocalDate.of(2022, 7, 1));
+        expected.add(LocalDate.parse("2022-07-02"));
+
+        Assertions.assertEquals(expected, res);
+    }
+
+    @ParameterizedTest(name = "numeroSecu {0} est valide : {1}")
+    @CsvSource({
+            "'2022-07-01','2022-07-02', 2"
+    })
+    void testcalculeJoursDeCongeDecomptesPourPlageEgalParama(String debut, String fin, double expectedNbJCongeDec) {
+        // Given :
+        SalarieAideADomicile aide = new SalarieAideADomicile("Jeanne",
+                LocalDate.now(), LocalDate.now(), 0, 0, 10, 1,
+                1);
+        // When :
+        LinkedHashSet<LocalDate> res = aide.calculeJoursDeCongeDecomptesPourPlage(LocalDate.parse(debut),
+                LocalDate.parse(fin));
+
+        // Then :
+
+        Assertions.assertEquals(expectedNbJCongeDec, res.size());
+    }
 }
